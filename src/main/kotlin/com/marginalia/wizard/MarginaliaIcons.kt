fc84d594bcd7be3151df80e8305406ecb1842476
Tim Janke <tim+github@janke.biz>
2026-02-26 12:16:29 +0100

    :sparkles: Add plugin icon and icon loader

---
+++ src/main/kotlin/com/marginalia/wizard/MarginaliaIcons.kt
@@ -0,0 +1,9 @@
+package com.marginalia.wizard
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object MarginaliaIcons {
+    @JvmField
+    val Marginalia: Icon = IconLoader.getIcon("/icons/marginalia.svg", MarginaliaIcons::class.java)
+}
